a78a1ddfaa35b5f9ef55057c68c5334a3a7cb560
dharmiksanthosh <dharmiksantosh@mail.com>
2020-10-02 20:02:46 +0530

    Initial Commit

---
+++ wall.js
@@ -0,0 +1,22 @@
+class Wall{
+    constructor(x, y, width, height, angle) {
+        var options = {
+            
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.body.angle = angle;
+        World.add(world, this.body);
+      }
+      display(){
+        var pos = this.body.position;
+        push();
+        rectMode(CENTER);
+        fill("brown");
+        noStroke();
+        rect(pos.x, pos.y, this.width, this.height);
+        pop();
+      }
+}
